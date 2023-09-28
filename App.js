d79a726dbc0e5f9d38eab9f9b43c06039dc99fd4
Thanhtung <thanhtung74159@gmail.com>
2023-09-28 11:48:34 +0700

    Thêm trang home

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+import HomeScreen from "./src/screens/HomeScreen";
+import MovieScreen from "./src/screens/MovieScreen";
+const Stack = createStackNavigator()
 
-export default function App() {
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={HomeScreen} options={{headerShown:false}}/>
+
+        <Stack.Screen name="movie" component={MovieScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import COLORS from "../containers/Colors"
+
+const HomeScreen = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <StatusBar style="auto" translucent={false} 
+      backgroundColor= {COLORS.BASIC_BACKGROUND} 
+      />
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>Now Playing</Text>
+        <Text style={styles.headerSubTitle}>View all</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BASIC_BACKGROUND,
+  },
+  headerContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  headerTitle:{
+    fontSize: 28,
+  },
+  headerSubTitle: {
+    fontSize: 18,
+    color: COLORS.ACTIVE
+  },
+});
+export default HomeScreen;
